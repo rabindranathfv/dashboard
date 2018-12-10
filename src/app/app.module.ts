d6b833c192b866ffd86b0a6140c39403e3843c2d
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-12-10 13:10:01 -0300

    adding structure of content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,29 @@ import { AppComponent } from './app.component';
 
 // Angular Material stuff
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule, MatToolbarModule, MatMenuModule, MatGridListModule, MatIconModule, MatCardModule } from '@angular/material';
+import {
+  MatCardModule,
+  MatGridListModule,
+  MatIconModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+
 
 // components
 import { NavbarComponent } from '../app/layouts/full/navbar/navbar.component';
 import { SidebarComponent } from '../app/layouts/full/sidebar/sidebar.component';
+import { HeaderComponent } from './layouts/material-components/header/header.component';
+import { SpinnerComponent } from './layouts/material-components/spinner/spinner.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
