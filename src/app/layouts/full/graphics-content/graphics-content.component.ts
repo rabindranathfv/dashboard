6b68765c2555fdbf728158c6a89018e5f2687e94
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-12-13 02:01:06 -0300

    draw some circle and rectangule, finish it

--- src/app/layouts/full/graphics-content/graphics-content.component.ts
+++ src/app/layouts/full/graphics-content/graphics-content.component.ts
@@ -16,14 +16,66 @@ export class GraphicsContentComponent implements OnInit, AfterContentInit {
     // using d3.js
   ngAfterContentInit() {
     // data test 0 2 8 1 2
-    d3.select('.col')
-    .append('svg')
-    .append('circle')
-    .attr('fill', 'red')
-    .attr('r', 100)
-    .attr('cx', 150)
-    .attr('cy', 150)
-    ;
+    let currentX = 100;
+    let currentY = 55;
+
+    let svg = d3.select('.card')
+                .append('svg')
+                // make zoom
+                .attr('transform', 'scale(1)')
+                .attr('margin-top', '20px')
+                ;
+    drawCircle(2);
+    drawCircle(8);
+    drawCircle(1);
+    drawCircle(2);
+
+    currentX = 455;
+    currentY += 20;
+
+    drawRectangule(2);
+    drawRectangule(8);
+    drawRectangule(1);
+    drawRectangule(2);
+
+    function drawRectangule(radious) {
+      svg
+        .append('rectangule')
+        .attr('fill', 'red')
+        .attr('width', 5)
+        .attr('height', radious)
+        .attr('x', currentX)
+        .attr('y', currentY)
+        ;
+      svg
+        .append('text')
+        .text(radious)
+        .attr('fill', 'black')
+        .attr('x', currentX)
+        .attr('y', currentY)
+        .attr('font-size', '20px')
+        ;
+      currentX += 25;
+    }
+
+    function drawCircle(radious: number) {
+      svg
+        .append('circle')
+        .attr('fill', 'red')
+        .attr('r', radious * 10)
+        .attr('cx', currentX)
+        .attr('cy', currentY)
+        ;
+      svg
+        .append('text')
+        .text(radious)
+        .attr('fill', 'black')
+        .attr('x', currentX)
+        .attr('y', currentY)
+        .attr('font-size', '3pt')
+        ;
+      currentX += 25;
+    }
   }
 
 }
