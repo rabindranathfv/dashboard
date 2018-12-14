0e30ec3b1c87e7c3098fb3014fd7a193292bb43a
Unknown <ferreiralox@gmail.com>
2018-12-14 15:02:43 -0300

    config for nvd3

--- src/app/layouts/full/graphics-content/graphics-content.component.ts
+++ src/app/layouts/full/graphics-content/graphics-content.component.ts
@@ -19,87 +19,8 @@ export class GraphicsContentComponent implements OnInit, AfterContentInit {
 
     // using d3.js
   ngAfterContentInit() {
-    // let data = [0, 2, 8, 1, 2 ];
-    // let initialX = 15;
-    // let deltaX = 25;
-    // let initialY = 15;
-    // let deltaY = 25;
-
-    // let currentX = initialX;
-    // let currentY = initialY;
-
-    // let svg = d3.select('#card1')
-    //             .append('svg')
-    //             // make zoom
-    //             // .attr('transform', 'scale(2)')
-    //             .attr('margin-top', '20px')
-    //             ;
-
-    // let gCircles = svg.append('g');
-    // gCircles.attr('class', 'circleGroup');
-
-    // data.forEach( val => drawCircle(gCircles, val));
-
-    // // drawCircle(gCircles, 2);
-    // // drawCircle(gCircles , 8);
-    // // drawCircle(gCircles, 1);
-    // // drawCircle(gCircles , 2);
-
-    // currentX = initialX;
-    // currentY += deltaY;
-
-    // let gRect = svg.append('g');
-    // gRect.attr('class', 'rectangleGroup');
-
-    // data.forEach( val => drawRectangule(gRect, val));
-    // // drawRectangule(gRect, 2);
-    // // drawRectangule(gRect, 8);
-    // // drawRectangule(gRect, 1);
-    // // drawRectangule(gRect, 2);
-
-    // function drawRectangule(gRect , radious) {
-    //   gRect
-    //     .append('rect')
-    //     .attr('fill', 'red')
-    //     .attr('width', 5)
-    //     .attr('height', radious)
-    //     .attr('x', currentX)
-    //     .attr('y', currentY)
-    //     ;
-    //   gRect
-    //     .append('text')
-    //     .text(radious)
-    //     .attr('fill', 'black')
-    //     .attr('x', currentX)
-    //     .attr('y', currentY)
-    //     .attr('font-size', '20px')
-    //     ;
-    //   currentX += deltaX;
-    // }
-
-    // function drawCircle(gCircles, radious: number) {
-    //   gCircles
-    //     .append('circle')
-    //     .attr('fill', 'red')
-    //     // conserve proportion on spaces for the figure
-    //     .attr('r', Math.sqrt(radious))
-    //     .attr('cx', currentX)
-    //     .attr('cy', currentY)
-    //     ;
-    //   gCircles
-    //     .append('text')
-    //     .text(radious)
-    //     .attr('fill', 'black')
-    //     .attr('x', currentX)
-    //     .attr('y', currentY)
-    //     .attr('font-size', '3pt')
-    //     ;
-    //   currentX += deltaX;
-    // }
-
-    //
     // Another example using D3
-    //
+    this.drawCirclesRect();
 
     const dataChart = [
       { date: '10/26/2018', value: 3 },
@@ -185,6 +106,77 @@ export class GraphicsContentComponent implements OnInit, AfterContentInit {
       const yAxis = d3.axisLeft(yAx);
       yAxis(yAxisGroup);
 
+  }
+
+  drawCirclesRect() {
+    let data = [0, 2, 8, 1, 2 ];
+     let initialX = 15;
+     let deltaX = 25;
+     let initialY = 15;
+     let deltaY = 25;
+    let currentX = initialX;
+     let currentY = initialY;
+    let svg = d3.select('#card1')
+                 .append('svg')
+                 // make zoom
+                 // .attr('transform', 'scale(2)')
+                 .attr('margin-top', '20px')
+                 ;
+    let gCircles = svg.append('g');
+     gCircles.attr('class', 'circleGroup');
+    data.forEach( val => drawCircle(gCircles, val));
+    // drawCircle(gCircles, 2);
+     // drawCircle(gCircles , 8);
+     // drawCircle(gCircles, 1);
+     // drawCircle(gCircles , 2);
+    currentX = initialX;
+     currentY += deltaY;
+    let gRect = svg.append('g');
+     gRect.attr('class', 'rectangleGroup');
+    data.forEach( val => drawRectangule(gRect, val));
+     // drawRectangule(gRect, 2);
+     // drawRectangule(gRect, 8);
+     // drawRectangule(gRect, 1);
+     // drawRectangule(gRect, 2);
+    function drawRectangule(gRect , radious) {
+       gRect
+         .append('rect')
+         .attr('fill', 'red')
+         .attr('width', 5)
+         .attr('height', radious)
+         .attr('x', currentX)
+         .attr('y', currentY)
+         ;
+       gRect
+         .append('text')
+         .text(radious)
+         .attr('fill', 'black')
+         .attr('x', currentX)
+         .attr('y', currentY)
+         .attr('font-size', '20px')
+         ;
+       currentX += deltaX;
+     }
+
+   function drawCircle(gCircles, radious: number) {
+     gCircles
+       .append('circle')
+       .attr('fill', 'red')
+       // conserve proportion on spaces for the figure
+       .attr('r', Math.sqrt(radious))
+       .attr('cx', currentX)
+       .attr('cy', currentY)
+       ;
+     gCircles
+       .append('text')
+       .text(radious)
+       .attr('fill', 'black')
+       .attr('x', currentX)
+       .attr('y', currentY)
+       .attr('font-size', '3pt')
+       ;
+     currentX += deltaX;
+   } 
   }
 
 }
